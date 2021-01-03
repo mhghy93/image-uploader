a2d5d8a3eba2a432880069bea7520eac1ea0cc13
mustaque <hussainmustaque98407@gmail.com>
2021-01-03 20:32:53 +0530

    define schema

---
+++ models/Image.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const imageSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  imageUrl: { type: String, required: true },
+  uploadedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Image', imageSchema);
